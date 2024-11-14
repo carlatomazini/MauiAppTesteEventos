b91d1230326404a959b5a240b8d6972d21265378
carlatomazini <tomazini.contato@gmail.com>
2024-11-14 00:31:19 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTesteEventos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppTesteEventos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
